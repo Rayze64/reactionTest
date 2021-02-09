2ce48b9d9325184d113fc17a8b1477c606014f68
Rayze64 <raymond.parts64@gmail.com>
2021-02-09 14:31:39 +0200

    Removed debounce

--- main.py
+++ main.py
@@ -15,28 +15,30 @@ lbl.place(x=60, y=10)
 btn = Button(window, text="Start", fg='black', font=("Helvetica", 16))
 btn.place(x=100, y=50)
 
-debounce = False
+# debounce = False
+
 
 def clicked():
     if lbl.text == "Click!":
         print("a")
 
+
 def wait():
     time.sleep(random.randint(1, 5))
     window.configure(bg='#00cc00')
     lbl.configure(text="Click!", fg='white', bg='#00cc00')
-    debounce = False
+    # debounce = False
 
 
 def begin(event):
-    if not debounce:
-        debounce = True
-        print(event)
-        window.configure(bg='#3366cc')
-        lbl.configure(text="Wait for green", fg='white', bg='#3366cc')
+    # if not debounce:
+    # debounce = True
+    print(event)
+    window.configure(bg='#3366cc')
+    lbl.configure(text="Wait for green", fg='white', bg='#3366cc')
 
-        x = threading.Thread(target=wait)
-        x.start()
+    x = threading.Thread(target=wait)
+    x.start()
 
 
 btn.bind("<Button-1>", begin)
